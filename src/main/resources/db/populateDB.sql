4ab32c2497fd22cc35b6b9eb8323ab01ee822165
andrewponomarev <andrewponomarev@yahoo.com>
2019-06-27 19:49:46 +0300

    4_8 add hsqldb

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -11,3 +11,7 @@ INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_ADMIN', 100001);
 
 
+INSERT INTO bets (event, value, currency, return_sum, net_profit, date_time, is_express, user_id)
+VALUES ('Admin bet 1', 0.0, '123', 0.0, 0.0, 0.0,'2015-05-30 10:00:00', true, 100001),
+       ('Admin bet 2', 0.0, '123', 0.0, 0.0, 0.0,'2015-05-30 20:00:00', true, 100001);
+
