f050fbab5de7657f26124178042c2d1c92cfdb88
andrewponomarev <andrewponomarev@yahoo.com>
2019-08-15 02:47:35 +0300

    i18n

--- src/main/java/ru/betanalysis/util/exception/ErrorType.java
+++ src/main/java/ru/betanalysis/util/exception/ErrorType.java
@@ -1,12 +1,10 @@
 package ru.betanalysis.util.exception;
 
 public enum ErrorType {
-
     APP_ERROR("error.appError"),
     DATA_NOT_FOUND("error.dataNotFound"),
     DATA_ERROR("error.dataError"),
-    VALIDATION_ERROR("error.validationError"),
-    WRONG_REQUEST("error.wrongRequest");
+    VALIDATION_ERROR("error.validationError");
 
     private final String errorCode;
 
